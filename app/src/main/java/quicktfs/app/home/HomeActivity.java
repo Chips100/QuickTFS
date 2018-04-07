c3b86ef708c1128836c79e481ee43096e1f48e14
Dennis <dennis.janiak@googlemail.com>
2018-04-07 19:44:32 +0200

    Add simple home screen with an input field to open Work Items directly by their ID.

--- app/src/main/java/quicktfs/app/home/HomeActivity.java
+++ app/src/main/java/quicktfs/app/home/HomeActivity.java
@@ -1,31 +1,46 @@
 package quicktfs.app.home;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import quicktfs.app.R;
+import quicktfs.app.workItemDetails.WorkItemDetailsActivity;
 
+/**
+ * Home screen for logged in user with starting points for all operations.
+ */
 public class HomeActivity extends AppCompatActivity {
+    // UI references.
+    private EditText searchWorkItemIdEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Get UI references.
+        searchWorkItemIdEditText = (EditText)findViewById(R.id.homeSearchWorkItemId);
     }
 
+    /**
+     * Opens a Work Item by its ID, specified by the user in the EditText.
+     * @param view View that raised the event.
+     */
+    public void openWorkItemById(View view) {
+        try {
+            int workItemId = Integer.parseInt(searchWorkItemIdEditText.getText().toString());
+
+            Intent intent = new Intent(this, WorkItemDetailsActivity.class);
+            intent.putExtra(WorkItemDetailsActivity.INTENT_WORKITEM_ID, workItemId);
+            startActivity(intent);
+        }
+        catch(NumberFormatException exception) {
+            Toast.makeText(this,
+                this.getString(R.string.homeSearchWorkItemIdMustBeNumberMessage), Toast.LENGTH_LONG).show();
+        }
+    }
 }
